77bee0eb59722055a6caa4160a5982b2a929d65f
David Volquartz Lebech <david@lebech.info>
2016-03-01 19:13:30 +0100

    Only read contents when cache is empty

--- test/asset-helper.js
+++ test/asset-helper.js
@@ -1,9 +1,21 @@
 'use strict';
 
 var AssetHelper = require('../lib/asset-helper.js'),
-    assert = require('assert');
+  assert = require('assert'),
+  sinon = require('sinon'),
+  fs = require('fs');
 
 describe('asset-helper', function() {
+
+  var sandbox;
+  beforeEach(function() {
+    sandbox = sinon.sandbox.create();
+  });
+
+  afterEach(function() {
+    sandbox.restore();
+  });
+
   it('should not accept appendHash flag without a baseDirectory', function() {
     try {
       new AssetHelper({
@@ -40,6 +52,20 @@ describe('asset-helper', function() {
       });
 
       assert.equal('https://media.receiptful.com/fixtures/file?v=ed797865eeb815b19a9e87746109c7c3', assetHelper.path('fixtures/file'));
+    });
+
+    it('should not read the file contents if the hash is cached', function() {
+      var fsSpy = sandbox.spy(fs, 'readFileSync');
+
+      var assetHelper = new AssetHelper({
+        baseUrl: 'https://media.receiptful.com/',
+        baseDirectory: __dirname + '/',
+        appendHash: true
+      });
+
+      assetHelper.path('fixtures/file2');
+      assetHelper.path('fixtures/file2');
+      sinon.assert.calledOnce(fsSpy);
     });
   });
 });
